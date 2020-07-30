5ec6be29bc11b52374f897605b541d582106b2fb
ijlee2 <ijlee2@users.noreply.github.com>
2020-07-30 09:04:34 -0500

    Updated ember-source to 3.20.2

--- config/targets.js
+++ config/targets.js
@@ -1,18 +1,10 @@
 'use strict';
 
-const browsers = [
-  'last 1 Chrome versions',
-  'last 1 Firefox versions',
-  'last 1 Safari versions'
-];
-
-const isCI = !!process.env.CI;
-const isProduction = process.env.EMBER_ENV === 'production';
-
-if (isCI || isProduction) {
-  browsers.push('ie 11');
-}
-
 module.exports = {
-  browsers
+  browsers: [
+    'Chrome >= 78',
+    'Edge >= 18',
+    'Firefox >= 75',
+    'Safari >= 13'
+  ]
 };
